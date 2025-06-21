db611e52a415ed6577505f7f2e0b26fbb0e241ee
RevEnterprise <169867921+RevEnterprise@users.noreply.github.com>
2025-06-21 20:40:19 +0700

    Update Bplus.cpp

--- Bplus.cpp
+++ Bplus.cpp
@@ -1,181 +1,390 @@
-// Searching on a B+ tree in C++
-
+// C++ Program to Implement B+ Tree
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <string>
 using namespace std;
 
-// node creation
-class Node {
+// B plus tree class
+template <typename T> class BPlusTree {
 public:
-    int order;
-    vector<string> values;
-    vector<Node*> children; // for internal nodes
-    vector<vector<string>> keys; // for leaf nodes
-    Node* nextKey;
-    Node* parent;
-    bool check_leaf;
-    Node(int order) {
-        this->order = order;
-        this->nextKey = nullptr;
-        this->parent = nullptr;
-        this->check_leaf = false;
+    // structure to create a node
+    struct Node {
+        bool isLeaf;
+        vector<T> keys;
+        vector<Node*> children;
+        Node* next;
+
+        Node(bool leaf = false)
+            : isLeaf(leaf)
+            , next(nullptr)
+        {
+        }
+    };
+
+    Node* root;
+    // Minimum degree (defines the range for the number of
+    // keys)
+    int t;
+
+    // Function to split a child node
+    void splitChild(Node* parent, int index, Node* child);
+
+    // Function to insert a key in a non-full node
+    void insertNonFull(Node* node, T key);
+
+    // Function to remove a key from a node
+    void remove(Node* node, T key);
+
+    // Function to borrow a key from the previous sibling
+    void borrowFromPrev(Node* node, int index);
+
+    // Function to borrow a key from the next sibling
+    void borrowFromNext(Node* node, int index);
+
+    // Function to merge two nodes
+    void merge(Node* node, int index);
+
+    // Function to print the tree
+    void printTree(Node* node, int level);
+
+public:
+    BPlusTree(int degree): root(nullptr), t(degree){}
+
+    void insert(T key);
+    bool search(T key);
+    void remove(T key);
+    vector<T> rangeQuery(T lower, T upper);
+    void printTree();
+};
+
+// Implementation of splitChild function
+template <typename T>
+void BPlusTree<T>::splitChild(Node* parent, int index,
+                              Node* child)
+{
+    Node* newChild = new Node(child->isLeaf);
+    parent->children.insert(
+        parent->children.begin() + index + 1, newChild);
+    parent->keys.insert(parent->keys.begin() + index,
+                        child->keys[t - 1]);
+
+    newChild->keys.assign(child->keys.begin() + t,
+                          child->keys.end());
+    child->keys.resize(t - 1);
+
+    if (!child->isLeaf) {
+        newChild->children.assign(child->children.begin()
+                                      + t,
+                                  child->children.end());
+        child->children.resize(t);
     }
-    
-    // insert at the leaf
-    void insert_at_leaf(Node* leaf, string value, string key) {
-        if (!values.empty()) {
-            for (int i = 0; i < values.size(); i++) {
-                if (value == values[i]) {
-                    keys[i].push_back(key);
-                    break;
+
+    if (child->isLeaf) {
+        newChild->next = child->next;
+        child->next = newChild;
+    }
+}
+
+// Implementation of insertNonFull function
+template <typename T>
+void BPlusTree<T>::insertNonFull(Node* node, T key)
+{
+    if (node->isLeaf) {
+        node->keys.insert(upper_bound(node->keys.begin(),
+                                      node->keys.end(),
+                                      key),
+                          key);
+    }
+    else {
+        int i = node->keys.size() - 1;
+        while (i >= 0 && key < node->keys[i]) {
+            i--;
+        }
+        i++;
+        if (node->children[i]->keys.size() == 2 * t - 1) {
+            splitChild(node, i, node->children[i]);
+            if (key > node->keys[i]) {
+                i++;
+            }
+        }
+        insertNonFull(node->children[i], key);
+    }
+}
+
+// Implementation of remove function
+template <typename T>
+void BPlusTree<T>::remove(Node* node, T key)
+{
+    // If node is a leaf
+    if (node->isLeaf) {
+        auto it = find(node->keys.begin(), node->keys.end(),
+                       key);
+        if (it != node->keys.end()) {
+            node->keys.erase(it);
+        }
+    }
+    else {
+        int idx = lower_bound(node->keys.begin(),
+                              node->keys.end(), key)
+                  - node->keys.begin();
+        if (idx < node->keys.size()
+            && node->keys[idx] == key) {
+            if (node->children[idx]->keys.size() >= t) {
+                Node* predNode = node->children[idx];
+                while (!predNode->isLeaf) {
+                    predNode = predNode->children.back();
                 }
-                else if (value < values[i]) {
-                    values.insert(values.begin() + i, value);
-                    keys.insert(keys.begin() + i, vector<string>{key});
-                    break;
+                T pred = predNode->keys.back();
+                node->keys[idx] = pred;
+                remove(node->children[idx], pred);
+            }
+            else if (node->children[idx + 1]->keys.size()
+                     >= t) {
+                Node* succNode = node->children[idx + 1];
+                while (!succNode->isLeaf) {
+                    succNode = succNode->children.front();
                 }
-                else if (i + 1 == values.size()) {
-                    values.push_back(value);
-                    keys.push_back(vector<string>{key});
-                    break;
-                }
+                T succ = succNode->keys.front();
+                node->keys[idx] = succ;
+                remove(node->children[idx + 1], succ);
+            }
+            else {
+                merge(node, idx);
+                remove(node->children[idx], key);
             }
         }
         else {
-            values.push_back(value);
-            keys.push_back(vector<string>{key});
-        }
-    }
-};
-
-// B+ tree
-class BplusTree {
-public:
-    Node* root;
-    BplusTree(int order) {
-        root = new Node(order);
-        root->check_leaf = true;
-    }
-    
-    // insert operation
-    void insert(string value, string key) {
-        Node* old_node = search(value);
-        old_node->insert_at_leaf(old_node, value, key);
-        if (old_node->values.size() == old_node->order) {
-            Node* node1 = new Node(old_node->order);
-            node1->check_leaf = true;
-            node1->parent = old_node->parent;
-            int mid = ceil(old_node->order / 2.0) - 1;
-            node1->values.assign(old_node->values.begin() + mid + 1, old_node->values.end());
-            node1->keys.assign(old_node->keys.begin() + mid + 1, old_node->keys.end());
-            node1->nextKey = old_node->nextKey;
-            old_node->values.resize(mid + 1);
-            old_node->keys.resize(mid + 1);
-            old_node->nextKey = node1;
-            insert_in_parent(old_node, node1->values[0], node1);
-        }
-    }
-    
-    // search operation for different operations
-    Node* search(string value) {
-        Node* current_node = root;
-        while (!current_node->check_leaf) {
-            for (int i = 0; i < current_node->values.size(); i++) {
-                if (value == current_node->values[i]) {
-                    current_node = current_node->children[i + 1];
-                    break;
+            if (node->children[idx]->keys.size() < t) {
+                if (idx > 0
+                    && node->children[idx - 1]->keys.size()
+                           >= t) {
+                    borrowFromPrev(node, idx);
                 }
-                else if (value < current_node->values[i]) {
-                    current_node = current_node->children[i];
-                    break;
+                else if (idx < node->children.size() - 1
+                         && node->children[idx + 1]
+                                    ->keys.size()
+                                >= t) {
+                    borrowFromNext(node, idx);
                 }
-                else if (i + 1 == current_node->values.size()) {
-                    current_node = current_node->children[i + 1];
-                    break;
-                }
-            }
-        }
-        return current_node;
-    }
-    
-    // find the node
-    bool find(string value, string key) {
-        Node* l = search(value);
-        for (int i = 0; i < l->values.size(); i++) {
-            if (l->values[i] == value) {
-                for (int j = 0; j < l->keys[i].size(); j++) {
-                    if (l->keys[i][j] == key) {
-                        return true;
+                else {
+                    if (idx < node->children.size() - 1) {
+                        merge(node, idx);
+                    }
+                    else {
+                        merge(node, idx - 1);
                     }
                 }
             }
-        }
-        return false;
-    }
-    
-    // inserting at the parent
-    void insert_in_parent(Node* n, string value, Node* ndash) {
-        if (root == n) {
-            Node* rootNode = new Node(n->order);
-            rootNode->values.push_back(value);
-            rootNode->children.push_back(n);
-            rootNode->children.push_back(ndash);
-            root = rootNode;
-            n->parent = rootNode;
-            ndash->parent = rootNode;
-            return;
-        }
-        Node* parentNode = n->parent;
-        for (int i = 0; i < parentNode->children.size(); i++) {
-            if (parentNode->children[i] == n) {
-                parentNode->values.insert(parentNode->values.begin() + i, value);
-                parentNode->children.insert(parentNode->children.begin() + i + 1, ndash);
-                if (parentNode->children.size() > parentNode->order) {
-                    Node* parentdash = new Node(parentNode->order);
-                    parentdash->parent = parentNode->parent;
-                    int mid = ceil(parentNode->order / 2.0) - 1;
-                    parentdash->values.assign(parentNode->values.begin() + mid + 1, parentNode->values.end());
-                    parentdash->children.assign(parentNode->children.begin() + mid + 1, parentNode->children.end());
-                    string value_ = parentNode->values[mid];
-                    parentNode->values.resize(mid);
-                    parentNode->children.resize(mid + 1);
-                    insert_in_parent(parentNode, value_, parentdash);
-                }
-                break;
-            }
+            remove(node->children[idx], key);
         }
     }
-    
-    // display the tree
-    void printTree(Node* node) {
-        if (node == nullptr) return;
-        for (int i = 0; i < node->values.size(); i++) {
-            cout << node->values[i] << " ";
+}
+
+// Implementation of borrowFromPrev function
+template <typename T>
+void BPlusTree<T>::borrowFromPrev(Node* node, int index)
+{
+    Node* child = node->children[index];
+    Node* sibling = node->children[index - 1];
+
+    child->keys.insert(child->keys.begin(),
+                       node->keys[index - 1]);
+    node->keys[index - 1] = sibling->keys.back();
+    sibling->keys.pop_back();
+
+    if (!child->isLeaf) {
+        child->children.insert(child->children.begin(),
+                               sibling->children.back());
+        sibling->children.pop_back();
+    }
+}
+
+// Implementation of borrowFromNext function
+template <typename T>
+void BPlusTree<T>::borrowFromNext(Node* node, int index)
+{
+    Node* child = node->children[index];
+    Node* sibling = node->children[index + 1];
+
+    child->keys.push_back(node->keys[index]);
+    node->keys[index] = sibling->keys.front();
+    sibling->keys.erase(sibling->keys.begin());
+
+    if (!child->isLeaf) {
+        child->children.push_back(
+            sibling->children.front());
+        sibling->children.erase(sibling->children.begin());
+    }
+}
+
+// Implementation of merge function
+template <typename T>
+void BPlusTree<T>::merge(Node* node, int index)
+{
+    Node* child = node->children[index];
+    Node* sibling = node->children[index + 1];
+
+    child->keys.push_back(node->keys[index]);
+    child->keys.insert(child->keys.end(),
+                       sibling->keys.begin(),
+                       sibling->keys.end());
+    if (!child->isLeaf) {
+        child->children.insert(child->children.end(),
+                               sibling->children.begin(),
+                               sibling->children.end());
+    }
+
+    node->keys.erase(node->keys.begin() + index);
+    node->children.erase(node->children.begin() + index
+                         + 1);
+
+    delete sibling;
+}
+
+// Implementation of printTree function
+template <typename T>
+void BPlusTree<T>::printTree(Node* node, int level)
+{
+    if (node != nullptr) {
+        for (int i = 0; i < level; ++i) {
+            cout << "  ";
+        }
+        for (const T& key : node->keys) {
+            cout << key << " ";
         }
         cout << endl;
-        if (!node->check_leaf) {
-            for (int i = 0; i <= node->values.size(); i++) {
-                printTree(node->children[i]);
-            }
+        for (Node* child : node->children) {
+            printTree(child, level + 1);
         }
     }
-};
+}
 
-int main() {
-    int record_len = 3;
-    BplusTree bplustree(record_len);
-    bplustree.insert("5", "33");
-    bplustree.insert("15", "21");
-    bplustree.insert("25", "31");
-    bplustree.insert("35", "41");
-    bplustree.insert("45", "10");
-    bplustree.printTree(bplustree.root);
-    if (bplustree.find("5", "34")) {
-        cout << "Found" << endl;
-    } else {
-        cout << "Not found" << endl;
+// Implementation of printTree wrapper function
+template <typename T> void BPlusTree<T>::printTree()
+{
+    printTree(root, 0);
+}
+
+// Implementation of search function
+template <typename T> bool BPlusTree<T>::search(T key)
+{
+    Node* current = root;
+    while (current != nullptr) {
+        int i = 0;
+        while (i < current->keys.size()
+               && key > current->keys[i]) {
+            i++;
+        }
+        if (i < current->keys.size()
+            && key == current->keys[i]) {
+            return true;
+        }
+        if (current->isLeaf) {
+            return false;
+        }
+        current = current->children[i];
     }
+    return false;
+}
+
+// Implementation of range query function
+template <typename T>
+vector<T> BPlusTree<T>::rangeQuery(T lower, T upper)
+{
+    vector<T> result;
+    Node* current = root;
+    while (!current->isLeaf) {
+        int i = 0;
+        while (i < current->keys.size()
+               && lower > current->keys[i]) {
+            i++;
+        }
+        current = current->children[i];
+    }
+    while (current != nullptr) {
+        for (const T& key : current->keys) {
+            if (key >= lower && key <= upper) {
+                result.push_back(key);
+            }
+            if (key > upper) {
+                return result;
+            }
+        }
+        current = current->next;
+    }
+    return result;
+}
+
+// Implementation of insert function
+template <typename T> void BPlusTree<T>::insert(T key)
+{
+    if (root == nullptr) {
+        root = new Node(true);
+        root->keys.push_back(key);
+    }
+    else {
+        if (root->keys.size() == 2 * t - 1) {
+            Node* newRoot = new Node();
+            newRoot->children.push_back(root);
+            splitChild(newRoot, 0, root);
+            root = newRoot;
+        }
+        insertNonFull(root, key);
+    }
+}
+
+// Implementation of remove function
+template <typename T> void BPlusTree<T>::remove(T key)
+{
+    if (root == nullptr) {
+        return;
+    }
+    remove(root, key);
+    if (root->keys.empty() && !root->isLeaf) {
+        Node* tmp = root;
+        root = root->children[0];
+        delete tmp;
+    }
+}
+
+// Main function to test the B+ Tree implementation
+int main()
+{
+    BPlusTree<int> tree(3);
+
+    // Insert elements
+    tree.insert(10);
+    tree.insert(20);
+    tree.insert(5);
+    tree.insert(15);
+    tree.insert(25);
+    tree.insert(30);
+
+    cout << "B+ Tree after insertions:" << endl;
+    tree.printTree();
+
+    // Search for a key
+    int searchKey = 15;
+    cout << "\nSearching for key " << searchKey << ": "
+         << (tree.search(searchKey) ? "Found" : "Not Found")
+         << endl;
+
+    // Perform a range query
+    int lower = 10, upper = 25;
+    vector<int> rangeResult = tree.rangeQuery(lower, upper);
+    cout << "\nRange query [" << lower << ", " << upper
+         << "]: ";
+    for (int key : rangeResult) {
+        cout << key << " ";
+    }
+    cout << endl;
+
+    // Remove a key
+    int removeKey = 20;
+    tree.remove(removeKey);
+    cout << "\nB+ Tree after removing " << removeKey << ":"
+         << endl;
+    tree.printTree();
+
     return 0;
 }
